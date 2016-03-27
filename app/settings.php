5f93655ef43e9f2baceb618417b05160629ed1a7
Aleksandar Savanovic <djaca85@gmail.com>
2016-03-27 20:49:44 +0200

    Added view and monolog settings

--- app/settings.php
+++ app/settings.php
@@ -5,5 +5,22 @@ return [
         'determineRouteBeforeAppMiddleware' => false,
         'displayErrorDetails' => true,
         'debug' => true,
+
+        // View settings
+        'view' => [
+            'template_path' => __DIR__ . '/View',
+            'twig' => [
+                'cache' => false,
+//                'cache' => __DIR__ . '/../cache/twig',
+                'debug' => true,
+                'auto_reload' => true,
+            ],
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'app',
+            'path' => __DIR__ . '/../log/app.log',
+        ],
     ],
 ];
