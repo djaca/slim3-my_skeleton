e89ee932c508bf46926802c059753931bcf0366d
Aleksandar Savanovic <djaca85@gmail.com>
2016-03-27 20:48:30 +0200

    Require files added

--- app/start.php
+++ app/start.php
@@ -9,3 +9,15 @@
     // Instantiate the app
     $settings = require __DIR__ . '/settings.php';
     $app = new \Slim\App($settings);
+
+    // Set up dependencies
+    require __DIR__ . '/dependencies.php';
+
+    // Register middleware
+    require __DIR__ . '/middleware.php';
+
+    // Register routes
+    require __DIR__ . '/routes.php';
+
+    // Register database
+    require __DIR__ . '/database.php';
