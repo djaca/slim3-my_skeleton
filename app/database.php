70186bf1341a7526c9764abf8bfb2769cc9ae5eb
Aleksandar Savanovic <djaca85@gmail.com>
2016-03-27 20:46:06 +0200

    Boot Eloquent

---
+++ app/database.php
@@ -0,0 +1,18 @@
+<?php
+    use Illuminate\Database\Capsule\Manager as Capsule;
+
+    $capsule = new Capsule();
+
+    $capsule->addConnection([
+        'driver' 	=> getenv('DB_TYPE'),
+        'host' 		=> getenv('DB_HOST'),
+        'database'	=> getenv('DB_DATABASE'),
+        'username' 	=> getenv('DB_USERNAME'),
+        'password' 	=> getenv('DB_PASSWORD'),
+        'charset' 	=> getenv('DB_CHARSET'),
+        'collation' => getenv('DB_COLLATION'),
+        'prefix' 	=> getenv('DB_PREFIX'),
+    ]);
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
